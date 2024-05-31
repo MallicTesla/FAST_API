539342e5ff639e3678fd319d7a80fa53ccdf2007
Mallic Tesla <el_mallic@hotmail.com>
2024-05-31 18:30:44 -0300

    para no perder la racha mientras travajo en otro proyecto

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def read_root():
 
 
 @app.get ("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item (item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.put("/items/{item_id}")
